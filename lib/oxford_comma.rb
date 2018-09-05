a20088526342bd867bbf6328a8b1404429401731
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-09-05 16:40:48 +0100

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+if array.length < 2
+  array[0]
+elsif array.length == 2
+  array.join(' and ')
+else
+  not_final = array[0..(array.length-2)]
+  return "#{not_final.join(', ')}, and #{array.last}"
+  end
 end
