4298a1ae8601956f644aa8460f571920430dcb1a
Sang Song <sangyoon.song@gmail.com>
2018-09-09 11:08:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 0 || array.length == 1
+    return array.join
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    arr = ", and #{array.pop}"
+    str = array.join(", ")
+    
+    return str + arr
+  end
 end
