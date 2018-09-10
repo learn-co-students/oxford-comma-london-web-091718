d5f92bd567adf7dea03dcad26e0ad0d8e0c1cf7d
Barney Loosemore <flatiron@barney.co.uk>
2018-09-10 14:10:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,19 +2,12 @@ def oxford_comma(array)
   case array.length
   when 1
     array.join('')
-    exit
   when 2 
     array.join (' and ')
-    exit
-  when 3
-    array[0,1].join(', ')
-    array[2].join('and ')
-    exit
   else 
     last_element = array.pop
     array.push("and #{last_element}")
     array.join(', ')
-    exit
   end
 end
 
