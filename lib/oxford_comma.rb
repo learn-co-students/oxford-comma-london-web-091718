30b3f701a0e659caeb4e75d74138f60b37fb137e
Barney Loosemore <flatiron@barney.co.uk>
2018-09-10 13:53:29 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join('')
+    exit
+  when 2 
+    array.join (' and ')
+    exit
+  when 3
+    array[0,1].join(', ')
+    array[2].join('and ')
+    exit
+  else 
+    last_element = array.pop
+    array.join(', ')
+    array.push("and #{last_element}")
+    exit
+  end
 end
+
